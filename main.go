a697f493244ce11ba74d2256d3c611cbd4b29d92
Shogo Iwano <shiwano@gmail.com>
2016-03-11 22:53:00 +0900

    :hourglass:

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	sdl.Init(sdl.INIT_EVERYTHING)
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	sdl.Delay(1000)
+	sdl.Quit()
+}
