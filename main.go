9933eb8e49703f1067d7bb088f1feef938a5c37c
Shogo Iwano <shiwano@gmail.com>
2016-03-11 23:07:29 +0900

    :hourglass:

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/veandco/go-sdl2/sdl"
 	"os"
 )
@@ -14,6 +15,32 @@ func main() {
 		panic(err)
 	}
 	defer window.Destroy()
+
+	eventChan := make(chan sdl.Event)
+	go func() {
+		for {
+			if event := sdl.PollEvent(); event != nil {
+				eventChan <- event
+			}
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case event := <-eventChan:
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				break loop
+			case *sdl.MouseMotionEvent:
+				fmt.Printf("[%d ms] MouseMotion\ttype:%d\tid:%d\tx:%d\ty:%d\txrel:%d\tyrel:%d\n",
+					t.Timestamp, t.Type, t.Which, t.X, t.Y, t.XRel, t.YRel)
+			case *sdl.MouseButtonEvent:
+				fmt.Printf("[%d ms] MouseButton\ttype:%d\tid:%d\tx:%d\ty:%d\tbutton:%d\tstate:%d\n",
+					t.Timestamp, t.Type, t.Which, t.X, t.Y, t.Button, t.State)
+			}
+		}
+	}
 
 	sdl.Delay(1000)
 	sdl.Quit()
